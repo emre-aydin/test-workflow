5e9cdd4a32748beb78bedaf7a57a47e572b06886
Emre Aydin <emre.aydin@zapatacomputing.com>
2022-08-24 16:50:41 +0300

    Try having an external dependency in the workflow

--- welcome.py
+++ welcome.py
@@ -4,6 +4,7 @@ This function saves a welcome message.
 
 import json
 import time
+import requests
 
 def welcome():
     message = "Welcome to Orquestra!"
@@ -11,8 +12,11 @@ def welcome():
     message_dict = {}
     message_dict["message"] = message
     message_dict["schema"] = "message"
+    r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+    message_dict["status_code"] = r.status_code
 
-    while True: time.sleep(1.0)
+    while True:
+        time.sleep(1.0)
 
     with open("welcome.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
