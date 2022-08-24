54819adc28eb7c7519ba296e62f2b3f3bfdfeb73
Emre Aydin <emre.aydin@zapatacomputing.com>
2022-08-24 17:14:13 +0300

    Remove indefinite wait and try the new python_requirements feature

--- welcome.py
+++ welcome.py
@@ -3,7 +3,6 @@ This function saves a welcome message.
 """
 
 import json
-import time
 import requests
 
 def welcome():
@@ -14,9 +13,6 @@ def welcome():
     message_dict["schema"] = "message"
     r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
     message_dict["status_code"] = r.status_code
-
-    while True:
-        time.sleep(1.0)
 
     with open("welcome.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
