b5f66730262699b8e6903adc067de85bee7b79b6
Emre Aydin <emre.aydin@zapatacomputing.com>
2022-08-25 14:10:15 +0300

    Use pyfiglet

--- welcome.py
+++ welcome.py
@@ -3,7 +3,8 @@ This function saves a welcome message.
 """
 
 import json
-import numpy as np
+import pyfiglet
+
 
 def welcome():
     message = "Welcome to Orquestra!"
@@ -12,10 +13,7 @@ def welcome():
     message_dict["message"] = message
     message_dict["schema"] = "message"
 
-    x = np.arange(15, dtype=np.int64).reshape(3, 5)
-    x[1:, ::2] = -99
-    max_per_row = x.max(axis=1)
-    message_dict["max_per_row"] = max_per_row
+    print(pyfiglet.figlet_format("Test workflow"))
 
     with open("welcome.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
